4f0c495866a7304daad278cc299f524cb83fed99
Mark Hollenbeck <hollenbeck.mark@gmail.com>
2014-05-28 10:58:24 -0500

    strip whitespace from submitted form values

--- bosphorus/forms.py
+++ bosphorus/forms.py
@@ -2,21 +2,25 @@ from flask_wtf import Form
 from wtforms import TextField, PasswordField, SelectField, DateField
 from wtforms import validators
 
+strip_filter = lambda x: x.strip() if x else None
+
 class LoginForm(Form):
     username = TextField(u'username',     validators=[validators.required()])
     password = PasswordField(u'password', validators=[validators.required()])
 
-
-def date_validator():
+def ssn_validator():
     return validators.Regexp('(^\d{3}-?\d{2}-?\d{4}$|^XXX-XX-XXXX$)')
 
 class PersonForm(Form):
-    research_id = SelectField(u'Research ID', validators=[validators.required()])
-    clinical_id = TextField(u'Clinical ID', validators=[validators.required()])
-    first_name  = TextField(u'First Name')
-    last_name   = TextField(u'Last Name')
+    research_id = SelectField(u'Research ID',
+                               validators=[validators.required()])
+    clinical_id = TextField(u'Clinical ID',
+                              validators = [validators.required()],
+                              filters = [strip_filter])
+    first_name  = TextField(u'First Name',filters=[strip_filter])
+    last_name   = TextField(u'Last Name',filters=[strip_filter])
     dob         = DateField(u'Date-of-birth', 
                             validators = [validators.Optional()],
                             format='%m/%d/%Y')
-    ssn         = TextField(u'SSN', validators = [date_validator(),
-                                                  validators.Optional()])
+    ssn         = TextField(u'SSN',
+                             validators = [ssn_validator(), validators.Optional()])
